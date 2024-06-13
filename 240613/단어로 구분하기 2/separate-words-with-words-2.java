0736dce6775d2f25f79638b0d45fcf1392fe4e49
Jeong Bo Yun <132794328+boyunee@users.noreply.github.com>
2024-06-13 15:25:25 +0900

    Title: 단어로 구분하기 2, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240613/단어로 구분하기 2/separate-words-with-words-2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String[] strings = new String[10];
+        for(int i =0; i<10; i++){
+            strings[i] = sc.next();
+            if(i % 2 == 0){
+                System.out.println(strings[i]);
+            }
+        }
+
+    }
+}
