e2433f51a7201dba519bcb1ed510619273112397
Jeong Bo Yun <132794328+boyunee@users.noreply.github.com>
2024-06-15 23:29:20 +0900

    Title: 문자 교체하기, Time: 270ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240615/문자 교체하기/changing-char.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2= sc.next();
+        System.out.println(str1.substring(0,2) + str2.substring(2));
+    }
+}
