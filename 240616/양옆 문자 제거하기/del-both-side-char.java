e29d42a5f8847f31e6b2ff6b2ccc1b59e988ba2f
Jeong Bo Yun <132794328+boyunee@users.noreply.github.com>
2024-06-16 15:50:15 +0900

    Title: 양옆 문자 제거하기, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240616/양옆 문자 제거하기/del-both-side-char.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(str.substring(0,1) +
+        str.substring(2,str.length() -2)+str.substring(str.length() -1));
+    }
+}
