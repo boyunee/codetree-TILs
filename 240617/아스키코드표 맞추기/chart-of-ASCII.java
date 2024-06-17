00ffb6c32be90fe233ad4e670f3cf2bdd02d49fe
Jeong Bo Yun <132794328+boyunee@users.noreply.github.com>
2024-06-17 17:26:19 +0900

    Title: 아스키코드표 맞추기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240617/아스키코드표 맞추기/chart-of-ASCII.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n =0;
+    
+        for(int i=0; i<5; i++){
+            n = sc.nextInt();
+            System.out.print((char)n);
+            System.out.print(" ");
+
+        }
+    }
+}
