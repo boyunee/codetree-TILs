5b50bf66fd3aa4ccecb97e32f34c2fb07cacd957
Jeong Bo Yun <132794328+boyunee@users.noreply.github.com>
2024-06-21 13:41:16 +0900

    Title: 1부터 특정 수까지의 합, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240621/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(print(n));
+        
+    }
+    public static int print(int n){
+        int sum =0;
+        for(int i=1; i<=n;i++){
+            sum += i;
+
+
+        }
+        return sum / 10;
+
+    }
+}
