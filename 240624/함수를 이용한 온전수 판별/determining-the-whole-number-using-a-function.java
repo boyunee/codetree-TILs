45498347e42830e006d95124539901252d8ba006
Jeong Bo Yun <132794328+boyunee@users.noreply.github.com>
2024-06-24 15:59:12 +0900

    Title: 함수를 이용한 온전수 판별, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240624/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int count =0;
+        for(int i=a; i<b; i++){
+            if(check(i)){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+    public static boolean check(int num ){
+
+        if((num % 2 == 0) || (num % 10 == 5)||((num % 3 ==0) && (num % 9 != 0))){
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
