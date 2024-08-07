38a14069a5a5cbe7c3e1b2b92c7be4b6e559d96e
Jeong Bo Yun <132794328+boyunee@users.noreply.github.com>
2024-08-07 14:07:10 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240807/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -0,0 +1,54 @@
+import java.util.*;
+public class Main {
+    public static int[] ints;
+    public static int result=1;
+    public static int find(int n){
+        if(n==0){
+            for(int i=0; i<ints.length; i++){
+                result *= ints[i];
+            }
+            return result;
+        }
+
+        if(ints[n]==1){
+            return find(n-1);
+        }
+        for(int i=2;  i<ints[n]; i++){
+            int count =0;
+            for(int j=0; j<ints.length; j++){
+                if(ints[j] % i == 0){
+                    count++;
+                    if(count >=2){
+                        result *=i;
+                        divide(i);
+                        break;
+                    }
+                }
+
+            }
+        }
+        return find(n-1);
+    }
+    public static void divide(int m){
+        for(int i=0; i<ints.length; i++){
+            if(ints[i] %m ==0){
+                ints[i] /= m;
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ints = new int[n];
+        for(int i =0; i<n; i++){
+            ints[i] = sc.nextInt();
+        }
+        System.out.println(find(n-1));
+
+
+
+    }
+}
