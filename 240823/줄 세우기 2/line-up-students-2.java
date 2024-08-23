dba3ff037a9a807a4a846d482c6b80045bfad0ad
Jeong Bo Yun <132794328+boyunee@users.noreply.github.com>
2024-08-23 14:32:49 +0900

    Title: 줄 세우기 2, Time: 311ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240823/줄 세우기 2/line-up-students-2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Student[] students = new Student[n];
+        for(int i=0; i<n; i++){
+            students[i] = new Student(sc.nextInt(), sc.nextInt(), i+1);
+        }
+        
+        Arrays.sort(students, Comparator.comparing((Student student) -> student.height)
+        .thenComparing(student -> student.weight));
+
+        for(int i=0; i<n; i++){
+            System.out.print(students[i].height+" ");
+            System.out.print(students[i].weight+" ");
+            System.out.print(students[i].order+" ");
+            System.out.println();
+        }
+
+    }
+}
+class Student{
+    int height, weight, order;
+
+    public Student(int height, int weight, int order){
+        this.height = height;
+        this.weight = weight;
+        this.order = order;
+
+    }
+}
