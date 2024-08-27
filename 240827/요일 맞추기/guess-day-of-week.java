09806ee6295ada2c6b0f7776a92eec317d6c69bf
Jeong Bo Yun <132794328+boyunee@users.noreply.github.com>
2024-08-27 19:37:42 +0900

    Title: 요일 맞추기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240827/요일 맞추기/guess-day-of-week.java
+++ 240827/요일 맞추기/guess-day-of-week.java
@@ -3,7 +3,7 @@ public class Main {
     public static String[] days = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
     public static int[] monthDays = new int[]{0,31,28,31,30,31,30,31,31,30,31,30,31};
     public static int m1,m2,d1,d2;
-    public static int order= 0;
+    public static day =0;
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
@@ -11,7 +11,7 @@ public class Main {
         d1 = sc.nextInt();
         m2 = sc.nextInt();
         d2 = sc.nextInt();
-        int day = check();
+        check();
         while(day < 0){
             day += 7;
         }
@@ -19,25 +19,24 @@ public class Main {
         System.out.println(days[day]);
     }
     public static int check(){
-        int d = 0;
         if((m2-m1) < 0){
-            d = d2 - d1;
+            day = d2 - d1;
             while(m1 != m2){
-                d += monthDays[m1];
+                day += monthDays[m1];
                 m1--;
             }
-            return d;
+            return day;
         }else if((m2-m1) == 0){
             return (d2 - d1);
 
 
         }else{
-            d = d2 - d1;
+            day = d2 - d1;
             while(m2 != m1){
-                d += monthDays[m1];
+                day += monthDays[m1];
                 m1++;
             }
-            return d;
+            return day;
 
         }
 
