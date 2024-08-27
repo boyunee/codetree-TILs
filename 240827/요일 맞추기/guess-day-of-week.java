9a00bbbe678fe6584040ff68b8d7f1c40eec517d
Jeong Bo Yun <132794328+boyunee@users.noreply.github.com>
2024-08-27 19:37:49 +0900

    Title: 요일 맞추기, Time: 163ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240827/요일 맞추기/guess-day-of-week.java
+++ 240827/요일 맞추기/guess-day-of-week.java
@@ -3,7 +3,7 @@ public class Main {
     public static String[] days = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
     public static int[] monthDays = new int[]{0,31,28,31,30,31,30,31,31,30,31,30,31};
     public static int m1,m2,d1,d2;
-    public static day =0;
+    public static int day =0;
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
