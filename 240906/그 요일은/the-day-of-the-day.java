ac9cdd0aed6f9626e5ef228215f34ee1f1a92c0c
Jeong Bo Yun <132794328+boyunee@users.noreply.github.com>
2024-09-06 12:39:32 +0900

    Title: 그 요일은, Time: 163ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240906/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Main {
+    public static int m1,d1,m2,d2;
+    public static String A;
+    public static int days = 0;
+    public static int[] months = {0,31,29,31,30,31,30,31,31,30,31,30,31};
+    public static String[] week = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        m1 = sc.nextInt();
+        d1 = sc.nextInt();
+        m2 = sc.nextInt();
+        d2 = sc.nextInt();
+        A = sc.next();
+        
+        int check = check();
+        // System.out.println(check);
+        int temp = check - Arrays.asList(week).indexOf(A);
+        System.out.println(temp / 7 + 1);
+
+
+    }
+    static int check() {
+        
+        while(true){
+            if(m1==m2 && d1==d2){
+            return days;
+        }
+            d1++;
+            days++;
+        if(d1 > months[m1]){
+            d1 = 1;
+            m1++;
+        }
+        }
+    }
+
+}
