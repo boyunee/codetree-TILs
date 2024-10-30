614052100670d92bd5a4a51c751e69959a1b30ed
Jeong Bo Yun <132794328+boyunee@users.noreply.github.com>
2024-10-30 16:04:53 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 174ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 241030/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Main {
+    public static int[][] ints = new int[2000][2000];
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        lining(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt());
+        liningM(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt());
+
+        int count=0;
+        for(int i=0; i<ints.length; i++){
+            for(int j=0; j<ints.length; j++){
+                if(ints[i][j] ==1){
+                    count++;
+                    // System.out.println(i+" "+j);
+                }
+            }
+        }
+        System.out.println(count);
+
+
+    }
+    static void lining(int x1, int y1, int x2, int y2){
+        for(int i=x1; i<=x2; i++){
+            for(int j=y1; j<=y2; j++){
+                ints[i+1000][j+1000] = 1;
+            }
+        }
+    }
+
+    static void liningM(int x1, int y1, int x2, int y2){
+        for(int i=x1; i<=x2; i++){
+            for(int j=y1; j<=y2; j++){
+                ints[i+1000][j+1000] = 0;
+            }
+        }
+    }
+}
