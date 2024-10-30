d5312b890688c520a0515c4e7ae921aa88f3f7b4
Jeong Bo Yun <132794328+boyunee@users.noreply.github.com>
2024-10-31 00:05:38 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 192ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 241031/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -0,0 +1,53 @@
+import java.util.*;
+public class Main {
+    public static int[][] ints = new int[2000][2000];
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        lining(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt());
+        liningM(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt());
+
+        int xMin =0, xMax=0, yMin=0, yMax=0;
+        for(int i=0; i<ints.length; i++){
+            for(int j=0; j<ints.length; j++){
+                if(ints[i][j] == 1){
+                    // System.out.println(i+" "+j);
+                    if(xMin==0 && yMin==0){
+                        xMin=2000;
+                        yMin=2000;
+                    }
+                    if(i < xMin){
+                        xMin = i;
+                    }
+                    if(i > xMax){
+                        xMax = i;
+                    }
+                    if(j < yMin){
+                        yMin = j;
+                    }
+                    if(j > yMax){
+                        yMax = j;
+                    }
+                } 
+            }
+        }
+        // System.out.println(xMax +" "+ xMin+ " "+yMax +" "+ yMin);
+        System.out.println((xMax - xMin) * (yMax - yMin));
+    }
+    static void lining(int x1, int y1, int x2, int y2){
+        for(int i=x1; i<=x2; i++){
+            for(int j=y1; j<=y2; j++){
+                ints[i+1000][j+1000] = 1;
+            }
+        }
+    }
+
+    static void liningM(int x1, int y1, int x2, int y2){
+        for(int i=x1+1; i<x2; i++){
+            for(int j=y1+1; j<y2; j++){
+                ints[i+1000][j+1000] = 0;
+            }
+        }
+    }
+    
+}
