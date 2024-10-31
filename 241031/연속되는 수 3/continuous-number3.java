ceb1e35cd5fd42eab354f99dd3c6afebd93241be
Jeong Bo Yun <132794328+boyunee@users.noreply.github.com>
2024-10-31 17:34:23 +0900

    Title: 연속되는 수 3, Time: 192ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 241031/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] ints = new int[n];
+        int count=0;
+        int maxCount=0;
+        for(int i=0; i<n; i++){
+            ints[i] = sc.nextInt();
+            if(i==0 || ints[i]*ints[i-1]<0){
+                count =1;
+                if(count > maxCount){
+                    maxCount = count;
+                }
+            } else{
+                count++;
+                if(count > maxCount){
+                    maxCount = count;
+                }
+            }
+        }
+        System.out.println(maxCount);
+
+    }
+}
