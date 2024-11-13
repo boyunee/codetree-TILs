c09fe8b89e02a0e1c6467b3684d8012fa17ee1d8
Jeong Bo Yun <132794328+boyunee@users.noreply.github.com>
2024-11-13 23:26:14 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 116ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 241113/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class Main {
+    public static int[] dx = {0,1,0,-1};
+    public static int[] dy = {-1,0,1,0};
+    public static int[][] ints;
+    public static int n,m,dir,x,y;
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        ints = new int[n][m];
+        dir = 0;
+
+        int count =0;
+        x=0;
+        y=0;
+        while(count++ < n*m){
+            ints[x][y] = count;
+            x+=dx[dir];
+            y+=dy[dir];
+            if(!check(x,y)){
+                x-=dx[dir];
+                y-=dy[dir];
+                dir = (dir+1)%4;
+                x+=dx[dir];
+                y+=dy[dir];
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                System.out.print(ints[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+
+    }
+    static boolean check(int x, int y){
+        return  x>=0 && x<n && y>=0 && y<m && ints[x][y]== 0;
+    }
+}
