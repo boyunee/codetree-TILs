169a2804d1ecbecd6557a35d532f481d1b8b56e6
Jeong Bo Yun <132794328+boyunee@users.noreply.github.com>
2024-11-13 23:34:38 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 241113/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
+++ 241113/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Main {
-    public static int[] dx = {0,1,0,-1};
-    public static int[] dy = {-1,0,1,0};
+    public static int[] dx = {1,0,-1,0};
+    public static int[] dy = {0,1,0,-1};
     public static int[][] ints;
     public static int n,m,dir,x,y;
     public static void main(String[] args) {
@@ -17,6 +17,7 @@ public class Main {
         x=0;
         y=0;
         while(count++ < n*m){
+
             ints[x][y] = count;
             x+=dx[dir];
             y+=dy[dir];
