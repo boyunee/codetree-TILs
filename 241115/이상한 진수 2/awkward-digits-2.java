4951a25529b8b0d97bbdbf0b70637a4c9a79f521
Jeong Bo Yun <132794328+boyunee@users.noreply.github.com>
2024-11-15 18:47:48 +0900

    Title: 이상한 진수 2, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241115/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String str  = sc.next();
+
+        int max = 0;
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i) == '0'){
+                int dec = toDecimal(str.substring(0,i)+"1"+str.substring(i+1));
+                // System.out.println(dec);
+                max = Math.max(max, dec);
+            }
+        }
+        System.out.println(max);
+
+
+    }
+    static int toDecimal(String str){
+        int num = Integer.valueOf(str);
+        // System.out.println(num);
+        int result = 0;
+        int t = 1;
+        while(num > 0){
+            result += (num % 10) * t;
+            t *=2;
+            num /= 10;
+        }
+        // System.out.println(result);
+        return result;
+    }
+}
