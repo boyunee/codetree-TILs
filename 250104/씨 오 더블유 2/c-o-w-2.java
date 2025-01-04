d7767817a5c87687773740127a82ca6fcc6f70ff
Jeong Bo Yun <132794328+boyunee@users.noreply.github.com>
2025-01-04 16:45:26 +0900

    Title: 씨 오 더블유 2, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 250104/씨 오 더블유 2/c-o-w-2.java
+++ 250104/씨 오 더블유 2/c-o-w-2.java
@@ -9,7 +9,7 @@ public class Main {
         int count = 0;
         for(int i=0; i<n-2;i++){
             if(str.charAt(i)=='C'){
-                for(int j=i+1; j<n-2; j++){
+                for(int j=i+1; j<n-1; j++){
                     if(str.charAt(j)=='O'){
                         for(int l=j+1; l<n; l++){
                             if(str.charAt(l)=='W') count++;
