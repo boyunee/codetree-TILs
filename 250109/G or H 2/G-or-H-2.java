6da4135094cfad2f6ef6dc12042fa36eafca49ca
Jeong Bo Yun <132794328+boyunee@users.noreply.github.com>
2025-01-09 23:57:06 +0900

    Title: G or H 2, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 250109/G or H 2/G-or-H-2.java
+++ 250109/G or H 2/G-or-H-2.java
@@ -16,6 +16,9 @@ public class Main {
                 ints[x] = 2;
             }
         }
+        // for(int i=0; i<MAX; i++){
+        //     System.out.print(ints[i]);
+        // }
         
         //G,H만으로 이뤄질때
         maxLen = 0;
@@ -42,7 +45,9 @@ public class Main {
             // System.out.println(d);
             maxLen = Math.max(d, maxLen);
         }
+        
         // System.out.println(maxLen);
+        int Gcount =0, Hcount=0;
         //G,H의 갯수가 같이 이뤄질때
         for(int i=0; i<MAX; i++){
             if(ints[i] == 0){
@@ -50,21 +55,28 @@ public class Main {
             }
             sum = 0;
             d = 0;
-            int count = 0;
+            Gcount = 0;
+            Hcount = 0;
             for(int j=i; j<MAX; j++){
                 if(ints[j] == 0){
                     continue;
                 }          
                 sum += ints[j];
-                count++;
-                if((sum % 3 ==0) && (count % 2 == 0)){
-                    d = j-i;
+                if(ints[j] == 1){
+                    Gcount++;
+                }else{
+                    Hcount++;
                 }
+                if((sum % 3 ==0) && (Gcount == Hcount)){
+                    d = j-i;
+                }      
             }
+            
             maxLen = Math.max(d, maxLen);
+            
         }
-        System.out.println(maxLen);
-
-
+         System.out.println(maxLen);
     }
+        
+
 }
