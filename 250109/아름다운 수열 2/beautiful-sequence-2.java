214e0bfb3686b2dc2d1096df969f1421f132e397
Jeong Bo Yun <132794328+boyunee@users.noreply.github.com>
2025-01-09 14:34:52 +0900

    Title: 아름다운 수열 2, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 250109/아름다운 수열 2/beautiful-sequence-2.java
+++ 250109/아름다운 수열 2/beautiful-sequence-2.java
@@ -1,40 +1,7 @@
 import java.util.*;
 public class Main {
     public static int[] a,b;
-    public static int n,m,count;
-    public static boolean[] matched;
-    public static void permute(int start, int end){
-        if(start == end){
-            matches();
-        }
-        for(int i=start;i<=end; i++){
-            swap(start, i);
-            permute(start+1, end);
-            swap(start, i);  
-        }
-    }
-    public static void swap(int i, int j){
-        int temp = b[i];
-        b[i] = b[j];
-        b[j] = temp;
-    }
-    public static void matches(){
-        for(int i=0; i<=n-m; i++){
-            boolean match = true;
-            for (int o = 0; o < m; o++) {
-                if (a[i + o] != b[o]) {
-                    match = false;
-                    break;
-                }
-            }
-            if(match && !matched[i]){
-                matched[i] = true;
-                count++;
-            }
-                       
-        }
-        
-    }
+    public static int n,m,count;   
 
     public static void main(String[] args) {
         // Please write your code here.
@@ -43,7 +10,7 @@ public class Main {
         m = sc.nextInt();
         a = new int[n];
         b = new int[m];
-        matched = new boolean[n];
+        int[] temp = new int[m];
         for(int i =0; i<n; i++){
             a[i] = sc.nextInt();
         }
@@ -55,7 +22,24 @@ public class Main {
             System.exit(0);
         }
         count =0;
-        permute(0, m-1);
+        Arrays.sort(b, 0, m);
+        for(int i=0; i<=n-m; i++){
+            for(int j=0; j<m; j++){
+                temp[j] = a[i+j];
+            }
+            Arrays.sort(temp, 0, m);
+            boolean match = true;
+            for(int j=0; j<m; j++){
+                if(temp[j] != b[j]){
+                    match = false;
+                    break;
+                }
+            }
+            if(match){
+                    count++;
+                }
+        }
+        
         System.out.println(count);
     }
 }
