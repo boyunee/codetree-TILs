8f5cf6f35ee803b7673c9bbab350d410a4aa4ffb
Jeong Bo Yun <132794328+boyunee@users.noreply.github.com>
2025-01-10 13:21:18 +0900

    Title: 한 가지로 열리는 자물쇠, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250110/한 가지로 열리는 자물쇠/one-way-lock.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+    public static int n,a,b,c;
+
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+        int unlock = 0;
+        for(int i=1 i<=n; i++){
+            for(int j=1; j<=n; j++){
+                for(int k=1; k<=n; k++){
+                    if((i >= a+3) && (j >= b+3) && (k >= c+3)){
+                        unlock++;
+                    }
+                }
+            }
+        }
+
+        System.out.println((int) Math.pow(n, 3) - unlock);
+    }
+}
